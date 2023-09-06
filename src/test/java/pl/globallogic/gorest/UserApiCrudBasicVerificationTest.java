2b1864ff189d95b32b076a396a5669bbb9e08577
Jakub Bujak <kuba.bujak0@wp.pl>
2023-09-06 22:33:00 +0200

    Configured api-test repo with some api tests

---
+++ src/test/java/pl/globallogic/gorest/UserApiCrudBasicVerificationTest.java
@@ -0,0 +1,108 @@
+package pl.globallogic.gorest;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pl.globallogic.gorest.model.CreateUserRequestDTO;
+
+import static io.restassured.RestAssured.*;
+
+public class UserApiCrudBasicVerificationTest extends BaseApiTest{
+
+    private static final String ENDPOINT = "/users";
+    private static String token = "56ad20c9a5e919880722cc3700318ae818d253da99b7aff59b328e9b303ce777";
+    private String ourUserId;
+
+    // token
+    // fixtures jak w python
+
+    @BeforeMethod
+    public void testSetUp() {
+        String randomEmail = getRandomEmail();
+        CreateUserRequestDTO userPayload =
+                new CreateUserRequestDTO("Super User", randomEmail, "male", "active");
+        Response res = given().
+                body(userPayload).
+                header("Authorization", "Bearer " + token).
+                contentType(ContentType.JSON).
+        when().
+                post(ENDPOINT).
+        then().extract().response();
+        ourUserId = res.jsonPath().getString("id");
+        logger.info("Created user id: {}", ourUserId);
+    }
+
+    // should fetch all users
+    @Test
+    public void shouldFetchAllUsersFromDefaultPage() {
+        given().
+                log().all().
+        when().
+                get(ENDPOINT).
+        then().
+                log().all();
+    }
+    // should list user data using user id
+    @Test
+    public void userDataShouldContainId() {
+        String userId = ourUserId;
+        given().
+                pathParam("userId", userId).
+                header("Authorization", "Bearer " + token).
+        when().
+                get( ENDPOINT + "/{userId}").
+        then().
+                log().all();
+    }
+
+    // should create new user and return id
+    private static String getRandomEmail() {
+        String email = "superuser." + (int)(Math.random() * 2000) + "@gmail.com";
+        logger.info("Generated email: {}", email);
+        return email;
+    }
+    @Test
+    public void shouldCreateUserAndReturnId() {
+        String randomEmail = getRandomEmail();
+        CreateUserRequestDTO userPayload =
+                new CreateUserRequestDTO("Super User", randomEmail, "male", "active");
+        given().
+                body(userPayload).
+                header("Authorization", "Bearer " + token).
+                contentType(ContentType.JSON).
+        when().
+                post(ENDPOINT).
+        then().
+                log().all();
+
+    }
+    // should update info with new information
+    @Test
+    public void shouldUpdateExistingUserWithNewData() {
+        String randomEmail = getRandomEmail();
+        CreateUserRequestDTO userPayload =
+                new CreateUserRequestDTO("Super User", randomEmail, "male", "active");
+        given().
+                pathParam("userId", ourUserId).
+                body(userPayload).
+                header("Authorization", "Bearer " + token).
+                contentType(ContentType.JSON).
+        when().
+                put(ENDPOINT + "/{userId}").
+        then().
+                log().all();
+    }
+    // should delete user from system
+    @Test
+    public void shouldDeleteExistingUserUsingId() {
+        Response res = given().
+                pathParam("userId", ourUserId).
+                header("Authorization", "Bearer " + token).
+        when().
+                delete(ENDPOINT + "/{userId}");
+        int expectedStatusCode = 204;
+        Assert.assertEquals(res.statusCode(), expectedStatusCode);
+    }
+}
---
+++ src/test/java/pl/globallogic/gorest/model/CreateUserRequestDTO.java
@@ -0,0 +1,5 @@
+package pl.globallogic.gorest.model;
+
+public record CreateUserRequestDTO(String name, String email, String gender, String status) {
+
+}
